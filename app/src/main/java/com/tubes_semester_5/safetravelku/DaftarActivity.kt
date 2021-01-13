5ad03c3457e37a018b7f4043e2e68c9fb715be28
Panji AM <72533509+PanjiAM@users.noreply.github.com>
2021-01-13 09:33:56 +0700

    Conect to Firebase
    Conect to Firebase

--- app/src/main/java/com/tubes_semester_5/safetravelku/DaftarActivity.kt
+++ app/src/main/java/com/tubes_semester_5/safetravelku/DaftarActivity.kt
@@ -1,13 +1,61 @@
 package com.tubes_semester_5.safetravelku
 
+import android.app.ProgressDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.FirebaseAuth
 
 class DaftarActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_daftar)
-
-        supportActionBar?.hide()
+        val btn = findViewById<Button>(R.id.daftar)
+        btn.setOnClickListener(View.OnClickListener {
+            val user = findViewById<EditText>(R.id.username)
+            val inuser = user.text.toString()
+            val pass = findViewById<EditText>(R.id.password)
+            val inpass = pass.text.toString()
+            Log.d("SignUp", "Email is" + inuser)
+            Log.d("SignUp", "Password:$inpass")
+            if (inuser.isEmpty() || inpass.isEmpty()) {
+                Toast.makeText(this, "Masukan Email dan Password", Toast.LENGTH_SHORT).show()
+                return@OnClickListener
+            }
+            val progressDialog = ProgressDialog(
+                this,
+                R.style.Theme_MaterialComponents_Light_Dialog
+            )
+            progressDialog.isIndeterminate = true
+            progressDialog.setMessage("Proses Mendaftar...")
+            progressDialog.show()
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(inuser, inpass)
+                .addOnCompleteListener {
+                    if (!it.isSuccessful) {
+                        return@addOnCompleteListener
+                        val intent = Intent(this, DaftarActivity::class.java)
+                        startActivity(intent)
+                    } else
+                        Log.d(
+                            "Sign Up",
+                            "Berhasil Mendaftarkan Akun dengan uid: ${it.result?.user?.uid}"
+                        )
+                    Toast.makeText(this, "Berhasil mendaftar akun", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this, MasukActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+                .addOnFailureListener {
+                    Log.d("Sign Up", "Gagal mendaftar akun${it.message}")
+                    Toast.makeText(this, "Email Sudah Terdaftar", Toast.LENGTH_SHORT).show()
+                    progressDialog.hide()
+                }
+        })
     }
 }
--- app/src/main/java/com/tubes_semester_5/safetravelku/MasukActivity.kt
+++ app/src/main/java/com/tubes_semester_5/safetravelku/MasukActivity.kt
@@ -3,18 +3,46 @@ package com.tubes_semester_5.safetravelku
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.FirebaseAuth
+import com.tubes_semester_5.safetravelku.databinding.ActivityMasukBinding
 import kotlinx.android.synthetic.main.activity_masuk.*
 
-class MasukActivity : AppCompatActivity() {
+class MasukActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_masuk)
+        val btn = findViewById<Button>(R.id.masuk)
+        btn.setOnClickListener(View.OnClickListener {
+            val user = findViewById<EditText>(R.id.username)
+            val inuser = user.text.toString()
+            val pass = findViewById<EditText>(R.id.password)
+            val inpass = pass.text.toString()
+            if (inuser.isEmpty() || inpass.isEmpty()) {
+                Toast.makeText(this, "Masukan Email dan Password", Toast.LENGTH_SHORT).show()
+                return@OnClickListener
+            }
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(inuser, inpass)
+                .addOnCompleteListener {
+                    if (!it.isSuccessful) {
+                        return@addOnCompleteListener
+                        val intent = Intent(this, MasukActivity::class.java)
+                        startActivity(intent)
+                    } else
+                        Toast.makeText(this, "Succesfully Login", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                }
+                .addOnFailureListener {
+                    Log.d("Main", "Failed Login: ${it.message}")
+                    Toast.makeText(this, "Email / Password Salah", Toast.LENGTH_SHORT).show()
+                }
+        })
 
-        supportActionBar?.hide()
-
-        masuk.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-        }
     }
 }
