750f9d62a9a5b0e1d3aef6584971b6ba8cf8c8ca
Panji AM <72533509+PanjiAM@users.noreply.github.com>
2021-02-14 20:43:07 +0700

    Activity SignOut done

--- app/src/main/java/com/tubes_semester_5/safetravelku/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/tubes_semester_5/safetravelku/ui/profile/ProfileFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat.finishAffinity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
@@ -13,6 +14,7 @@ import com.google.firebase.ktx.Firebase
 import com.tubes_semester_5.safetravelku.MasukActivity
 import com.tubes_semester_5.safetravelku.R
 import com.tubes_semester_5.safetravelku.databinding.FragmentProfileBinding
+import kotlinx.android.synthetic.main.fragment_profile.*
 
 
 class ProfileFragment : Fragment() {
@@ -20,12 +22,20 @@ class ProfileFragment : Fragment() {
     private lateinit var binding: FragmentProfileBinding
 
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
     }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        btnSignOut.setOnClickListener{
+            val intent = Intent(activity, MasukActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            startActivity(intent)
+            FirebaseAuth.getInstance().signOut()
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
